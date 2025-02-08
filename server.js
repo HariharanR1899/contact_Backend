cdc391880eee3d7172ba497544a00763bd38d00d
Hariharan R <hariharan.r1899@gmail.com>
2025-02-08 19:36:30 +0000

    adding user to logi response

--- server.js
+++ server.js
@@ -138,7 +138,13 @@ app.post("/login", async (req, res) => {
     if (!validPassword) return res.status(400).json({ error: "Invalid email or password" });
 
     const token = jwt.sign({ userId: user.rows[0].id }, JWT_SECRET, { expiresIn: "1h" });
-    res.json({ token });
+    res.json({ token,
+
+        user: { 
+            id: user.rows[0].id, 
+            email: user.rows[0].email 
+          } 
+     });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
